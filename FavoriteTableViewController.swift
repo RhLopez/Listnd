54aa43ee127e120e54628fa014d9d863c9b55512
Ramiro H. Lopez <rhl0583@gmail.com>
2016-10-02 23:43:37 -0700

    Add FavoriteTableViewController Add SearchViewController

---
+++ FavoriteTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteTableViewController.swift
+//  Listnd
+//
+//  Created by Ramiro H. Lopez on 10/2/16.
+//  Copyright © 2016 Ramiro H. Lopez. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewController: UITableViewController {
+    
+}
--- Listnd/SearchViewController.swift
+++ Listnd/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Listnd
 //
 //  Created by Ramiro H. Lopez on 10/2/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

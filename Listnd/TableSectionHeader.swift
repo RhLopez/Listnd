c6fcf3208302b674ce97efdc314e9080b3d9d9f8
RhLopez <ramiro.h.lopez@outlook.com>
2017-02-18 02:09:16 -0800

    add: TableSectionHeader subclass

---
+++ Listnd/TableSectionHeader.swift
@@ -0,0 +1,14 @@
+//
+//  TableSectionHeader.swift
+//  Listnd
+//
+//  Created by Ramiro H. Lopez on 2/17/17.
+//  Copyright © 2017 Ramiro H. Lopez. All rights reserved.
+//
+
+import UIKit
+
+class TableSectionHeader: UITableViewHeaderFooterView {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}

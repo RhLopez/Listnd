9fba7a0892a19bf80be5d0e664772d189d7a589a
RhLopez <ramiro.h.lopez@outlook.com>
2017-04-15 09:38:30 -0700

    Add addGradient()

--- Listnd/HeaderView.swift
+++ Listnd/HeaderView.swift
@@ -18,49 +18,66 @@ class HeaderView: GSKStretchyHeaderView {
     @IBOutlet weak var addButton: UIButton!
     
     var imageTemplate: UIImageView!
+    var gradientView: UIView!
     
     override func didChangeStretchFactor(_ stretchFactor: CGFloat) {
         super.didChangeStretchFactor(stretchFactor)
         var alpha: CGFloat = 1
-        if stretchFactor < 0.8 {
+        var gradientAlpha: CGFloat = 1
+        
+        if stretchFactor > 1 {
+            gradientAlpha = CGFloatTranslateRange(stretchFactor, 1, 1.03, 1, 0)
+        } else if stretchFactor < 0.8 {
             alpha = CGFloatTranslateRange(stretchFactor, 0.2, 1, 0, 1)
         }
+        
+        let navTitleFactor: CGFloat = 0.4
+        //var navTitleAlpha: CGFloat = 0
+        if stretchFactor < navTitleFactor {
+            //navTitleAlpha = CGFloatTranslateRange(stretchFactor, 0, navTitleFactor, 1, 0)
+        }
+        
         alpha = max(0, alpha)
-        imageView.alpha = alpha
+        backgroundImage.alpha = alpha
+        nameLabel.alpha = alpha
+        gradientView.alpha = gradientAlpha
+        //navigationLabel.alpha = navTitleAlpha
     }
     
     func configureView(name: String, imageData: Data?, hideButton: Bool) {
-        imageView.layer.shadowColor = UIColor.black.cgColor
-        imageView.layer.shadowOffset = CGSize(width: 0, height: 5)
-        imageView.layer.shadowOpacity = 0.7
-        imageView.layer.shadowRadius = 8.5
-        let borderFrame = UIView()
-        borderFrame.frame = imageView.bounds
-        borderFrame.layer.cornerRadius = 4.0
-        borderFrame.layer.masksToBounds = true
-        imageView.addSubview(borderFrame)
-        imageTemplate = UIImageView()
-        imageTemplate.frame = imageView.bounds
-        imageTemplate.contentMode = .scaleAspectFill
-        imageTemplate.clipsToBounds = true
-        borderFrame.addSubview(imageTemplate)
-        backgroundImage.image = UIImage(named: "backgroundImage")
         addButton.layer.cornerRadius = 3.0
         addButton.setTitleColor(UIColor.white, for: .normal)
         addButton.setTitleColor(UIColor.black, for: .highlighted)
         
+        nameLabel.numberOfLines = 0
+        nameLabel.lineBreakMode = .byWordWrapping
+        nameLabel.sizeToFit()
+        nameLabel.layer.cornerRadius = 4.0
+        nameLabel.layer.masksToBounds = true
         nameLabel.text = name
         if let data = imageData {
-            imageTemplate.image = UIImage(data: data)
+            backgroundImage.image = UIImage(data: data)
         } else {
-            imageTemplate.image = UIImage(named: "headerPlaceHolder")
+            backgroundImage.image = UIImage(named: "headerPlaceHolder")
         }
         
         addButton.isHidden = hideButton
+        addGradient()
     }
     
     func setImage(data: Data) {
         let image = UIImage(data: data)
-        UIView.transition(with: self.imageTemplate, duration: 1, options: .transitionCrossDissolve, animations: { self.imageTemplate.image = image }, completion: nil)
+        UIView.transition(with: self.backgroundImage, duration: 1, options: .transitionCrossDissolve, animations: { self.backgroundImage.image = image }, completion: nil)
+    }
+    
+    func addGradient() {
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: contentView.frame.height)
+        gradient.colors = [UIColor.black.cgColor, UIColor.clear.cgColor]
+        gradient.opacity = 0.30
+        gradient.locations = [0.0, 1.0]
+        gradientView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: contentView.frame.height))
+        gradientView.layer.addSublayer(gradient)
+        backgroundImage.addSubview(gradientView)
     }
 }
